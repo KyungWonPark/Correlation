e70cc6a85b5f421a54956748ba6e6aac9ac4c8a5
KyungWonPark <kw.park.person@gmail.com>
2020-10-21 10:44:49 +0900

    1234

--- cmd/c3/build.sh
+++ cmd/c3/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/^\/\/ "gonum.org/v1/gonum/blas/blas64"$/"gonum.org/v1/gonum/blas/blas64"/g' main.go
-sed -i 's/^\/\/ blas_netlib "gonum.org/v1/netlib/blas/netlib"$/blas_netlib "gonum.org/v1/netlib/blas/netlib"/g' main.go
+sed -i 's/^\/\/ "gonum.org\/v1\/gonum\/blas\/blas64"$/"gonum.org\/v1\/gonum\/blas\/blas64"/g' main.go
+sed -i 's/^\/\/ blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"$/blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"/g' main.go
 sed -i 's/^\/\/ blas64.Use(blas_netlib.Implementation{})$/blas64.Use(blas_netlib.Implementation{})/g' main.go
 
 export CGO_LDFLAGS="-L$ROOT/opt/openblas/lib -lopenblas"
