fd88d76ab504e25f9fde1e4dd8f6e45a0a4b1184
KyungWonPark <kw.park.person@gmail.com>
2020-10-27 17:42:51 +0900

    1234

--- cmd/smallScale/main.go
+++ cmd/smallScale/main.go
@@ -30,6 +30,7 @@ func main() { // thrStart thrEnd thrItv isDebugMode
 
 	DATADIR := os.Getenv("DATA")
 	RESULTDIR := os.Getenv("RESULT")
+	RESULTDIR = RESULTDIR + "/small"
 
 	numQueueSize := 8
 	ringBuffer := make([]*mat64.Dense, numQueueSize)
@@ -81,12 +82,12 @@ func main() { // thrStart thrEnd thrItv isDebugMode
 	fmt.Println("Writing C2-tilda")
 	io.Mat64toCSV(RESULTDIR+"/c2-tilda.csv", avgedMat)
 
-	matBufferShm, err := shm.Create(13362 * 13362 * 8)
+	matBufferShm, err := shm.Create(1000 * 1000 * 8)
 	if err != nil {
 		log.Fatalf("Failed to create shared memory region: %s\n", err)
 	}
 
-	eigValShm, err := shm.Create(13362 * 1 * 8)
+	eigValShm, err := shm.Create(1000 * 1 * 8)
 	if err != nil {
 		log.Fatalf("Failed to create shared memory region: %s\n", err)
 	}
@@ -103,33 +104,27 @@ func main() { // thrStart thrEnd thrItv isDebugMode
 
 	arrThredMat := make([]float64, 13362*13362)
 	thredMat := mat64.NewDense(13362, 13362, arrThredMat)
-	arrEigVal := make([]float64, 13362*1)
-	eigVal := mat64.NewDense(13362, 1, arrEigVal)
-	arrEigVec := make([]float64, 13362*13362)
-	eigVec := mat64.NewDense(13362, 13362, arrEigVec)
+	arrEigVal := make([]float64, 1000*1)
+	eigVal := mat64.NewDense(1000, 1, arrEigVal)
+	arrEigVec := make([]float64, 1000*1000)
+	eigVec := mat64.NewDense(1000, 1000, arrEigVec)
+
+	testMat := mat64.NewDense(1000, 1000, nil)
 
 	var thr float64
 	for thr = thrStart; thr < thrEnd; thr += thrItv {
 		fmt.Printf("Processing: thr - %f\n", thr)
 		pl.Threshold(avgedMat, thredMat, thr, 0)
-		pl.Laplacian(thredMat) // Now thredMat is a Laplacian matrix
 
-		// Check Symmetry
-		if isDebugMode {
-			for i := 0; i < 13362; i++ {
-				for j := 0; j < 13362; j++ {
-					if thredMat.At(i, j) != thredMat.At(j, i) {
-						log.Fatalf("Warning mat[%d][%d]: %f | mat[%d][%d]: %f\n", i, j, thredMat.At(i, j), j, i, thredMat.At(j, i))
-					}
-				}
-			}
-		}
+		getOnly1k(thredMat, testMat)
 
-		mat64tocArr(thredMat, pMatBuffer) // Copy thresholded matrix to MAGMA matrix buffer
+		pl.Laplacian(testMat) // Now thredMat is a Laplacian matrix
+
+		mat64tocArr(testMat, pMatBuffer) // Copy thresholded matrix to MAGMA matrix buffer
 
 		fmt.Printf("Diagonalizing...")
 		// Call MAGMA
-		cmd := exec.Command("files/magma", "13362", fmt.Sprintf("%d", matBufferShm.Id), fmt.Sprintf("%d", eigValShm.Id))
+		cmd := exec.Command("files/magma", "1000", fmt.Sprintf("%d", matBufferShm.Id), fmt.Sprintf("%d", eigValShm.Id))
 		err := cmd.Run()
 		if err != nil {
 			log.Fatalf("MAGMA execution has failed: %s\n", err)
@@ -142,23 +137,23 @@ func main() { // thrStart thrEnd thrItv isDebugMode
 		if isDebugMode {
 			fmt.Printf("Threshold: %f Checking results...\n", thr)
 
-			eigValMat := mat64.NewDense(13362, 13362, nil)
-			for i := 0; i < 13362; i++ {
+			eigValMat := mat64.NewDense(1000, 1000, nil)
+			for i := 0; i < 1000; i++ {
 				eigValMat.Set(i, i, eigVal.At(i, 0))
 			}
 
 			// U^T * A
-			result0 := mat64.NewDense(13362, 13362, nil)
-			result0.Mul(eigVec, thredMat)
+			result0 := mat64.NewDense(1000, 1000, nil)
+			result0.Mul(eigVec, testMat)
 
 			// S * U^T
-			result1 := mat64.NewDense(13362, 13362, nil)
+			result1 := mat64.NewDense(1000, 1000, nil)
 			result1.Mul(eigValMat, eigVec)
 
 			isSame := mat64.EqualApprox(result0, result1, 0.000001)
 			if !isSame {
 				fmt.Printf("Thr: %f / Eigen decomposition has failed!\n", thr)
-				diff := mat64.NewDense(13362, 13362, nil)
+				diff := mat64.NewDense(1000, 1000, nil)
 				diff.Sub(result0, result1)
 				log.Fatalf("Max(| U^T * A - S * U^T |) : %g\n", mat64.Max(diff))
 			}
@@ -177,6 +172,23 @@ func main() { // thrStart thrEnd thrItv isDebugMode
 
 	eigValShm.Detach(pEigVal)
 	eigValShm.Destroy()
+
+	return
+}
+
+func getOnly1k(thredMat *mat64.Dense, testMat *mat64.Dense) {
+	inputRows, inputCols := thredMat.Dims()
+	outputRows, outputCols := testMat.Dims()
+
+	if inputRows > outputCols || inputCols > outputCols {
+		log.Fatalf("[getOnly1k] Input matrix is smaller!")
+	}
+
+	for i := 0; i < outputRows; i++ {
+		for j := 0; j < outputCols; j++ {
+			testMat.Set(i, j, thredMat.At(i, j))
+		}
+	}
 
 	return
 }
