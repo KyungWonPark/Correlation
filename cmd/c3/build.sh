7a8075450ef7846556fcd2ed23e796378b6ed3e1
KyungWonPark <kw.park.person@gmail.com>
2020-10-29 14:41:34 +0900

    1234

--- cmd/c3/build.sh
+++ cmd/c3/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i 's/^\/\/ "gonum.org\/v1\/gonum\/blas\/blas64"$/"gonum.org\/v1\/gonum\/blas\/blas64"/g' main.go
-sed -i 's/^\/\/ blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"$/blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"/g' main.go
-sed -i 's/^\/\/ blas64.Use(blas_netlib.Implementation{})$/blas64.Use(blas_netlib.Implementation{})/g' main.go
+sed -i 's/\/\/ "gonum.org\/v1\/gonum\/blas\/blas64"/"gonum.org\/v1\/gonum\/blas\/blas64"/g' main.go
+sed -i 's/\/\/ blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"/blas_netlib "gonum.org\/v1\/netlib\/blas\/netlib"/g' main.go
+sed -i 's/\/\/ blas64.Use(blas_netlib.Implementation{})/blas64.Use(blas_netlib.Implementation{})/g' main.go
 
 cp ~/Downloads/magma-2.5.3/magma ./files/
 
