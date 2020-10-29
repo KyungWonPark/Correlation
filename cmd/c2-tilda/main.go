8e6e2196f31af4a8d335b80c898c52466c28df44
KyungWonPark <kw.park.person@gmail.com>
2020-10-29 17:34:26 +0900

    1234

---
+++ cmd/c2-tilda/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/KyungWonPark/Correlation/internal/calc"
+	"github.com/KyungWonPark/Correlation/internal/io"
+	"github.com/gonum/matrix/mat64"
+)
+
+func main() { // thrStart thrEnd thrItv isDebugMode
+	DATADIR := os.Getenv("DATA")
+	RESULTDIR := os.Getenv("RESULT")
+
+	numQueueSize := 8
+	ringBuffer := make([]*mat64.Dense, numQueueSize)
+	for i := 0; i < numQueueSize; i++ {
+		ringBuffer[i] = mat64.NewDense(13362, 600, nil)
+	}
+
+	pl := calc.Init(numQueueSize, false)
+
+	go func() {
+		for _, file := range fileList {
+			dest := pl.Malloc()
+			doSampling(DATADIR+"/fMRI-Smoothed/"+file, ringBuffer[dest], pl.GetNP())
+			pl.Push(dest)
+		}
+
+		pl.Close()
+		pl.StopScheduler()
+
+		return
+	}()
+
+	avgedMat := mat64.NewDense(13362, 13362, nil)
+
+	{
+		accedMat := mat64.NewDense(13362, 13362, nil)
+		{
+			temp0 := mat64.NewDense(13362, 600, nil)
+			temp1 := mat64.NewDense(13362, 600, nil)
+			temp2 := mat64.NewDense(13362, 13362, nil)
+
+			for {
+				job, ok := pl.Pop()
+				if ok {
+					pl.ZScoring(ringBuffer[job], temp0)
+					pl.Sigmoid(temp0, temp1)
+					pl.Pearson(temp1, temp2)
+					pl.Acc(temp2, accedMat)
+
+					pl.Free(job)
+				} else {
+					break
+				}
+			}
+		}
+		pl.Avg(accedMat, avgedMat, float64(len(fileList)))
+	}
+
+	fmt.Println("Writing C2-tilda")
+	io.Mat64toCSV(RESULTDIR+"/c2-tilda.csv", avgedMat)
+
+	return
+}
