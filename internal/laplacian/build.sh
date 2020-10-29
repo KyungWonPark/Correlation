ad242c030954d434dd1dd801b4355e849d584891
KyungWonPark <kw.park.person@gmail.com>
2020-10-29 21:03:29 +0900

    1234

--- internal/laplacian/build.sh
+++ internal/laplacian/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -O3 -fPIC -DNDEBUG -DADD_ -Wall -fopenmp -DMAGMA_ILP64 -DHAVE_CUBLAS -DMIN_CUDA_ARCH=700 -I/usr/local/cuda/include -I./files/include -I./files/testing -c -o files/laplacian.o laplacian.c
-ar -r files/liblaplacian.a files/laplacian.o
+gcc -O3 -fPIC -DNDEBUG -DADD_ -Wall -fopenmp -DMAGMA_ILP64 -DHAVE_CUBLAS -DMIN_CUDA_ARCH=700 -I/usr/local/cuda/include -I./include -I./testing -c -o laplacian.o laplacian.c
+ar -r liblaplacian.a laplacian.o
