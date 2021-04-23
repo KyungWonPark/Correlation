b7c88a9469ed7894f60b675718459358f7b1048a
KyungWonPark <kw.park.person@gmail.com>
2021-04-23 16:17:16 +0900

    added de-coarse graining

---
+++ cmd/de-coarse/init.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type VoxelCoord struct {
+	x      int
+	y      int
+	z      int
+	isGray bool
+	value  float64
+}
+
+type Voxel struct {
+	isGray bool
+	value  float64
+	denom  int
+}
+
+var seedList [13362]VoxelCoord
+var fineMap [91][109][91]Voxel
+
+var shareMap [4]int
+
+func init() {
+	shareMap[0] = 8
+	shareMap[1] = 4
+	shareMap[2] = 2
+	shareMap[3] = 1
+
+	f, err := os.Open("greyList.txt")
+	if err != nil {
+		log.Fatal("Failed to open file: greyList.txt", err)
+	}
+	defer f.Close()
+
+	i := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ",")
+		x, err0 := strconv.Atoi(line[0])
+		y, err1 := strconv.Atoi(line[1])
+		z, err2 := strconv.Atoi(line[2])
+		if err0 != nil || err1 != nil || err2 != nil {
+			log.Fatal("Failed to convert ascii to integer", err0, err1, err2)
+		}
+
+		seedList[i].x = 2*x - 1
+		seedList[i].y = 2*y - 1
+		seedList[i].z = 2 * z
+		seedList[i].isGray = true
+
+		i++
+	}
+
+	g, err := os.Open("TD_type.dat")
+	if err != nil {
+		log.Fatal("Failed to open file: TD_type.dat", err)
+	}
+	defer g.Close()
+
+	scanner = bufio.NewScanner(g)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		if line[3] == "1.000" {
+			x, err0 := strconv.Atoi(line[0])
+			y, err1 := strconv.Atoi(line[1])
+			z, err2 := strconv.Atoi(line[2])
+			if err0 != nil || err1 != nil || err2 != nil {
+				log.Fatal("Failed to convert ascii to integer", err0, err1, err2)
+			}
+
+			fineMap[x-1][y-1][z-1].isGray = true
+		}
+	}
+}
---
+++ cmd/de-coarse/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"sync"
+
+	"github.com/KyungWonPark/nifti"
+	"github.com/kshedden/gonpy"
+)
+
+func main() {
+	fileName := os.Args[1]
+	fmt.Printf("Filename: %s\n", fileName)
+
+	npyReader, err := gonpy.NewFileReader(fileName)
+	if err != nil {
+		log.Fatal("Cannot open npy file", err)
+	}
+
+	npyDim := npyReader.Shape[0]
+	npyDat, err := npyReader.GetFloat64()
+	if err != nil {
+		log.Fatal("Failed to read npy file", err)
+	}
+
+	for i := 0; i < npyDim; i++ {
+		seedList[i].value = npyDat[i]
+	}
+
+	for _, seed := range seedList {
+		for xD := -1; xD < 2; xD++ {
+			for yD := -1; yD < 2; yD++ {
+				for zD := -1; zD < 2; zD++ {
+					dist := xD*xD + yD*yD + zD*zD
+					share := shareMap[dist]
+
+					xPos := seed.x + xD
+					yPos := seed.y + yD
+					zPos := seed.z + zD
+
+					vox := &fineMap[xPos][yPos][zPos]
+					if vox.isGray {
+						vox.value += (seed.value * float64(share))
+						vox.denom += share
+					}
+				}
+			}
+		}
+	}
+
+	averaging(&fineMap)
+
+	newImg := nifti.NewImg(91, 109, 91, 1)
+	for xPos := 0; xPos < 91; xPos++ {
+		for yPos := 0; yPos < 109; yPos++ {
+			for zPos := 0; zPos < 91; zPos++ {
+				val := fineMap[xPos][yPos][zPos].value
+				newImg.SetAt(uint32(xPos), uint32(yPos), uint32(zPos), 0, float32(val))
+			}
+		}
+	}
+
+	newImg.Save("u.nii")
+}
+
+func avg0(fineMap *[91][109][91]Voxel, order <-chan int, wg *sync.WaitGroup) {
+	for {
+		xPos, ok := <-order
+		if ok {
+			for yPos := 0; yPos < 109; yPos++ {
+				for zPos := 0; zPos < 91; zPos++ {
+					vox := &fineMap[xPos][yPos][zPos]
+
+					if vox.denom != 0 {
+						val := vox.value
+						vox.value = val / float64(vox.denom)
+					}
+				}
+			}
+
+			wg.Done()
+		} else {
+			break
+		}
+	}
+}
+
+func avg1(fineMap *[91][109][91]Voxel, order <-chan int, wg *sync.WaitGroup) {
+	for {
+		xPos, ok := <-order
+		if ok {
+			for yPos := 0; yPos < 109; yPos++ {
+				for zPos := 0; zPos < 91; zPos++ {
+					vox := &fineMap[xPos][yPos][zPos]
+
+					if vox.denom != 0 {
+						val := vox.value
+						vox.value = val / float64(8)
+					}
+				}
+			}
+
+			wg.Done()
+		} else {
+			break
+		}
+	}
+}
+
+func averaging(fineMap *[91][109][91]Voxel) {
+	order := make(chan int, runtime.NumCPU())
+	var wg sync.WaitGroup
+
+	wg.Add(91)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go avg0(fineMap, order, &wg)
+	}
+
+	for xPos := 0; xPos < 91; xPos++ {
+		order <- xPos
+	}
+
+	wg.Wait()
+	close(order)
+}
